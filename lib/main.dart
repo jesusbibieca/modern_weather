ca170969f3befb8def886ed481cb8b1068f0b90e
Jesus Bibieca <jesusbibieca52@gmail.com>
2019-03-20 22:10:05 -0400

    Adding environmental variables

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  await DotEnv().load('.env');
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Modern Weather',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
